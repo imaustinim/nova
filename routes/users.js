47034d29e6500e7044a783a56a34d8d2aa41893b
Austin Im <imaustinim@gmail.com>
2021-03-06 16:54:56 -0500

    update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {id: 1, username: "sombone"},
+    {id: 2, username: "somboneadsf"},
+    {id: 3, username: "sombonea"},
+  ])
 });
 
 module.exports = router;
