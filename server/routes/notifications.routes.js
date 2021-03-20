ba340294c2604973fe05c62d932795961f500967
Austin Im <imaustinim@gmail.com>
2021-03-19 21:20:23 -0400

    rm

--- server/routes/notifications.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const notificationsCtrl = require("../controllers/notifications.controllers")
-
-router.get("/", notificationsCtrl.show);
-
-module.exports = router;
